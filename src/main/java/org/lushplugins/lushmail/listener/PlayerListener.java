505286c68e5a6c4737ad8eccdfa892f87123c70e
Oak <cooldaveb@live.co.uk>
2024-06-12 12:17:15 +0100

    Added mail notification on join

--- src/main/java/org/lushplugins/lushmail/listener/PlayerListener.java
+++ src/main/java/org/lushplugins/lushmail/listener/PlayerListener.java
@@ -1,8 +1,10 @@
 package org.lushplugins.lushmail.listener;
 
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.lushplugins.lushlib.libraries.chatcolor.ChatColorHandler;
 import org.lushplugins.lushlib.listener.EventListener;
 import org.lushplugins.lushmail.LushMail;
 
@@ -11,8 +13,15 @@ public class PlayerListener implements EventListener {
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event) {
         Player player = event.getPlayer();
-        LushMail.getInstance().getMailManager().getUnopenedGroupMailIds(player).thenAccept(mailIds -> {
-
-        });
+        Bukkit.getScheduler().runTaskLater(LushMail.getInstance(), () -> {
+            LushMail.getInstance().getMailManager().getAllUnopenedMailIds(player).thenAccept(mailIds -> {
+                int mailCount = mailIds.size();
+                if (mailCount > 0) {
+                    // TODO: Add configurable message
+                    ChatColorHandler.sendMessage(player, "You have %count% new mail"
+                        .replace("%count%", String.valueOf(mailCount)));
+                }
+            });
+        }, 40);
     }
 }
