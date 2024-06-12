801841b14ca6bed53e8b3c948892b82c11c61f97
Oak <cooldaveb@live.co.uk>
2024-06-12 20:56:41 +0100

    Added delete all command

--- src/main/java/org/lushplugins/lushmail/command/subcommand/DeleteMailCommand.java
+++ src/main/java/org/lushplugins/lushmail/command/subcommand/DeleteMailCommand.java
@@ -4,10 +4,13 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import org.lushplugins.lushlib.command.SubCommand;
 import org.lushplugins.lushlib.libraries.chatcolor.ChatColorHandler;
 import org.lushplugins.lushmail.LushMail;
 import org.lushplugins.lushmail.storage.StorageManager;
+
+import java.util.List;
 
 public class DeleteMailCommand extends SubCommand {
 
@@ -31,19 +34,42 @@ public class DeleteMailCommand extends SubCommand {
 
         String mailId = args[0].strip();
         StorageManager storageManager = LushMail.getInstance().getStorageManager();
-        storageManager.hasReceivedMail(player.getUniqueId(), mailId).thenAccept(received -> {
-            if (!received) {
-                // TODO: Add configurable message
-                ChatColorHandler.sendMessage(sender, "Could not find this mail in your mail list");
-                return;
-            }
+        if (!mailId.equals("all")) {
+            storageManager.hasReceivedMail(player.getUniqueId(), mailId).thenAccept(received -> {
+                if (!received) {
+                    // TODO: Add configurable message
+                    ChatColorHandler.sendMessage(sender, "Could not find this mail in your mail list");
+                    return;
+                }
 
-            storageManager.removeMailFor(player.getUniqueId(), mailId).thenAccept(ignored -> {
-                // TODO: Add configurable message
-                ChatColorHandler.sendMessage(sender, "Mail '" + mailId + "' has been removed from your mail list");
+                storageManager.removeMailFor(player.getUniqueId(), mailId).thenAccept(ignored -> {
+                    // TODO: Add configurable message
+                    ChatColorHandler.sendMessage(sender, "Mail '" + mailId + "' has been removed from your mail list");
+                });
             });
-        });
+        } else {
+            storageManager.getReceivedMailIds(player.getUniqueId(), mailId).thenAccept(ids -> {
+                if (ids.isEmpty()) {
+                    // TODO: Add configurable message
+                    ChatColorHandler.sendMessage(sender, "Could not find any mail in your mail list");
+                    return;
+                }
+
+                for (String id : ids) {
+                    // TODO: Consider adding remove all mail method
+                    storageManager.removeMailFor(player.getUniqueId(), id);
+                }
+
+                // TODO: Add configurable message
+                ChatColorHandler.sendMessage(sender, "Your mail list has been emptied");
+            });
+        }
 
         return true;
+    }
+
+    @Override
+    public @Nullable List<String> tabComplete(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args, @NotNull String[] fullArgs) {
+        return args.length == 1 ? List.of("all") : null;
     }
 }
